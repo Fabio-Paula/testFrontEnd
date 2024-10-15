58f2024a5727ea5ac49aace44eccdceb4eeeec43
Fabio Roberto de Paula <fabiopaula.dev@gmail.com>
2024-10-15 17:53:33 -0300

    criado componente da navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.less'],
+})
+export class NavbarComponent {
+  profileImg =
+    'https://s3-alpha-sig.figma.com/img/2983/00c3/f7c8a8ca2f16c4cbfd746902cb0f8d57?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EFb8I7xHcP1sn0QQGW-~XTyJgy3dihJPgEOukkbtTIa6fh7-RTIjstvtVQKFYBhfYis~ZOTnkz9IoExNdljeND5YXfVi6YAUiufx0PzEZDVuEofNLsNdog8fTBJKlm1pKhVQf1qlPzsiiWgSVlaZtgwDzVkeX1KMtfmC80ayAFxlTAJLBTos8OEFplNVp8jURg7tzZFcrMv-AKh4zctmsPkJ4aHtZp~qvjEaHWhi-HBf~eXWKkFRr4-pWwlL01M0u~FBzzjXpcR1dTK6KYLWQXuD3RXIAoomL18skDaOZkfnOqV~UM9sV7rNlHIH7~-eZp16P3wd4kfujAxi7UpnjQ__';
+}
