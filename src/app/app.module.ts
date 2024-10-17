06eb2a20430c753e22c3f453dcacf20d577b8a38
Fabio Roberto de Paula <fabiopaula.dev@gmail.com>
2024-10-17 17:51:02 -0300

    adicionado imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
 import { ChartModule } from 'primeng/chart';
+import { InputTextModule } from 'primeng/inputtext';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent],
-  imports: [BrowserModule, ChartModule],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, LoginComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ChartModule,
+    ButtonModule,
+    AppRoutingModule,
+    InputTextModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
