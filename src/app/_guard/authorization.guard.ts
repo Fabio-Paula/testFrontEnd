b12fc01c91c9af23b0aacc423b687971fd785810
Fabio Roberto de Paula <fabiopaula.dev@gmail.com>
2024-10-18 13:23:09 -0300

    console log removido do guard

--- src/app/_guard/authorization.guard.ts
+++ src/app/_guard/authorization.guard.ts
@@ -1,7 +1,6 @@
 import { CanActivateFn } from '@angular/router';
 
 export const authorizationGuard: CanActivateFn = (route, state) => {
-  console.log(route);
 
   const isLoggedIn = !!localStorage.getItem('login');
   if (!isLoggedIn) window.location.href = '/login';
